760a9cfee2f275e0dafe415de5fe7678d0fc3b20
TomTris <quoctrido.jb@gmail.com>
2024-08-14 21:32:52 +0200

    learn dataclass

---
+++ ex03/new_student.py
@@ -0,0 +1,23 @@
+import random
+import string
+from dataclasses import dataclass, field
+
+def generate_id() -> str:
+	return "".join(random.choices(string.ascii_lowercase, k = 15))
+
+@dataclass
+class Student:
+	name : str
+	surname : str
+	active : bool = True
+	login : str = "Eagle"
+	id : str = field(init=False, default_factory=generate_id)
+
+
+def main():
+	student = Student(name = "Edward", surname = "agle")
+	print(student)
+
+
+if __name__ == "__main__":
+	main()
