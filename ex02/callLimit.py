4bef9d0b79de19949593bf04f6236a4ef841947c
TomTris <quoctrido.jb@gmail.com>
2024-08-14 21:01:16 +0200

    learn wrappers

---
+++ ex02/callLimit.py
@@ -0,0 +1,30 @@
+def callLimit(limit: int):
+    count = 0
+    def callLimiter(function):
+        def limit_function(*args: any, **kwds: any):
+            nonlocal count
+            if (count < limit):
+                count += 1
+                val = function()
+                return val
+            else:
+                print(f"{function} call to many times")
+                return None
+        return limit_function
+    return callLimiter
+
+def main():
+    @callLimit(3)
+    def f():
+        print ("f()")
+
+    @callLimit(1)
+    def g():
+        print ("g()")
+
+    for i in range(3):
+        f()
+        g()
+
+if __name__ == "__main__":
+    main()
